00f70aab4cbf920a998c2394e6ddc7be3c0a79dd
Mike <mike.oli.silva1@gmail.com>
2023-03-25 20:18:38 -0300

    Adicionando instruções de inspeção

--- Passagem_Onibus.cpp
+++ Passagem_Onibus.cpp
@@ -8,7 +8,7 @@
 typedef struct 
 {
 	char nome[60], ida_volta[4];
-	string origem, destino;
+	int origem, destino;
 	long long int cpf;
 	int idade, ida_dia, ida_mes, volta_dia, volta_mes, hora_ida, hora_volta, banco;
 	float valor;
@@ -75,6 +75,7 @@ int main()
 		case 2:
 				
 			verificar(registrador, contador);
+			goto back1;
 			break;
 				
 		case 3:
@@ -149,13 +150,12 @@ void comprar_passagem(tipo_passagem *r, int c)
 	linha();
 	
 		// coleta do codigo da cidade de ORIGEM
-	int origem , destino;
 		
 	cout << "Codigo da origem -> ";
-	cin >> origem;
+	cin >> p.origem;
 	
 		// Verificação de existencia de cidade
-	if ( origem < 0 or origem > 7)
+	if ( p.origem < 0 or p.origem > 7)
 	{
 			
 		cout << "Codigo invalido, insira apenas os codigos da tabela" << endl;
@@ -165,12 +165,12 @@ void comprar_passagem(tipo_passagem *r, int c)
 			
 	}
 	
-		// coleta do codigo da cidade de ORIGEM
+		// coleta do codigo da cidade de DESTINO
 	cout << "Codigo do destino -> ";
-	cin >> destino;
+	cin >> p.destino;
 	
 		// Verificação de existencia de cidade		
-	if ( origem < 0 or origem > 7)
+	if ( p.destino < 0 or p.destino > 7)
 	{
 			
 		cout << "Codigo invalido, insira apenas os codigos da tabela" << endl;
@@ -181,18 +181,14 @@ void comprar_passagem(tipo_passagem *r, int c)
 	}
 		
 	cout << endl;
-	
-		// tranferencia das variaveis para a struct p
-	p.origem = cidades.cidades[origem];
-	p.destino = cidades.cidades[destino];
 		
 		// Verificação de cidades iguais
-	if ( origem == destino )
+	if ( p.origem == p.destino )
 	{
 		
 		linha();
 			
-		cout << "Invalido. Origem -> " << cidades.cidades[origem] << ", Destino -> " <<cidades.cidades[origem] << endl;
+		cout << "Invalido. Origem -> " << cidades.cidades[p.origem] << ", Destino -> " <<cidades.cidades[p.origem] << endl;
 			
 		linha();
 		
@@ -212,9 +208,9 @@ void comprar_passagem(tipo_passagem *r, int c)
 	system("cls");
 		
 		// Coleta das variaveis caso seja necessario
-	cout << "Digite o DIA de Ida [ Ex: 01, 02, 03 ...]:\n -> ";
+	cout << "Digite o DIA de Ida [ Ex: 1, 2, 3 ...]:\n -> ";
 	cin >> p.ida_dia;
-	cout << "Digite o MES de Ida [ Ex: 01 - janeiro, 02 - fevereiro ... ]:\n -> " << p.ida_dia << "/";
+	cout << "Digite o MES de Ida [ Ex: 1 - janeiro, 2 - fevereiro ... ]:\n -> " << p.ida_dia << "/";
 	cin >> p.ida_mes;
 		
 		back2:
@@ -259,9 +255,9 @@ void comprar_passagem(tipo_passagem *r, int c)
 			
 			system("cls");
 			
-		cout << "Digite o DIA de Volta[ Ex: 01, 02, 03 ... ]:\n -> ";
+		cout << "Digite o DIA de Volta[ Ex: 1, 2, 3 ... ]:\n -> ";
 		cin >> p.volta_dia;
-		cout << "Digite o MES de Volta [ Ex: 01 - janeiro, 02 - fevereiro ... ]:\n -> " << p.volta_dia << "/";
+		cout << "Digite o MES de Volta [ Ex: 1 - janeiro, 2 - fevereiro ... ]:\n -> " << p.volta_dia << "/";
 		cin >> p.volta_mes;
 			
 			back3:
@@ -303,7 +299,7 @@ void comprar_passagem(tipo_passagem *r, int c)
 			
 	int cont  = 1;
 			
-	for ( int a = 0; a < cont; a++ )
+	for ( int a = 0; a < c; a++ )
 	{
 			
 		if ( (p.origem == r[a].origem and p.destino == r[a].destino) and (p.hora_ida == r[a].hora_ida)) cont++;
@@ -312,8 +308,8 @@ void comprar_passagem(tipo_passagem *r, int c)
 		
 	p.banco = cont;
 	
-	if ( origem > destino ) p.valor = cidades.kms[destino][origem] * 0.60 + 20;
-	else p.valor = cidades.kms[origem][destino] * 0.60 + 20;
+	if ( p.origem > p.destino ) p.valor = cidades.kms[p.destino][p.origem] * 0.60 + 20;
+	else p.valor = cidades.kms[p.origem][p.destino] * 0.60 + 20;
 		
 	if ( p.ida_volta[0] == 'S')
 	{
@@ -344,10 +340,10 @@ void verificar(tipo_passagem *r, int c)
 		system("cls");
 		back1:
 	
-	long long int escolha;
-	
+	long long int cpf;
+
 	cout << "Digite seu CPF: \n -> ";
-	cin >> escolha;
+	cin >>cpf;
 	
 		system("cls");
 		linha();
@@ -357,7 +353,7 @@ void verificar(tipo_passagem *r, int c)
 	for(int i = 0; i < c; i++)
 	{
 		
-		if(r[i].cpf == escolha)
+		if(r[i].cpf == cpf)
 		{
 			
 			pos = i;
@@ -379,8 +375,114 @@ void verificar(tipo_passagem *r, int c)
 
 		
 	cout << endl << endl << " ---------- Informacões de viagem ---------- " << endl << endl;
-
+	Sleep(1000);
+	cout << "Seja bem vindo " << r[pos].nome << endl;
+	Sleep(1000);
+	cout << "Sua poutrona:  " << r[pos].banco << endl;
+	Sleep(1000);
+	cout << "--------------" << endl;
+	cout << " XX          I " << endl;
+	cout << "------  ------" << endl;
+		
+	int contador = r[pos].banco;
 	
+	for (int a = 0; a < 11; a++)
+	{
+		
+		cout << "| ";
+		if(contador == 1)
+		{
+			
+			cout << "O ";
+			contador = 44;
+			
+		}else
+		{
+			
+			cout << "X ";
+			contador--;	
+			
+		}
+		
+		if(contador == 1)
+		{
+			
+			cout << "O    ";
+			contador = 44;
+			
+		}else
+		{
+			
+			cout << "X    ";
+			contador--;	
+			
+		}
+		
+		if(contador == 1)
+		{
+			
+			cout << "O ";
+			contador = 44;
+			
+		}else
+		{
+			
+			cout << "X ";
+			contador--;	
+			
+		}
+		
+		if(contador == 1)
+		{
+			
+			cout << "O ";
+			contador = 44;
+			
+		}else
+		{
+			
+			cout << "X ";
+			contador--;	
+			
+		}
+		
+		cout << "|" << endl;;
+	
+		
+	}
+	cout << "--------------" << endl;
+	cout << "Descrição: \n I = Saida\n O = Banco reservado\n X = outros bancos\n XX = banco do condutor" << endl << endl;
+	Sleep(1000);
+	Sleep(1000);
+	cout << "Data da Ida: " << r[pos].ida_dia << "/" << r[pos].ida_mes << " - " << r[pos].hora_ida << ":00;" << endl;
+	
+	if(r[pos].ida_volta[0] == 'S')
+		cout << "Data da Volta: " << r[pos].volta_dia << "/" << r[pos].volta_mes << " - " << r[pos].hora_volta << ":00;" << endl;
+		
+	Sleep(1000);
+	tipo_cidades cidades;
+	cout << "Origem: " << cidades.cidades[r[pos].origem] << ";" << endl;
+	Sleep(1000);
+	cout << "Destino: " << cidades.cidades[r[pos].destino] << ";" << endl;
+	Sleep(1000);
+	cout << "Valor da passagem: R$" << r[pos].valor << endl << endl;
+	Sleep(1000);
+	cout << "** O valor da passagem deve ser pago somente ao embarcar no transporte.\n Desde ja agradecemos a preferencia :)\nDeseja voltar ao inicio[Sim/Nao]?" << endl;
+	char escolha[4];
+		back3:
+	fflush(stdin);
+	gets(escolha);
+	fflush(stdin);
+	
+	if(escolha[0] == 'S')
+	{
+		
+		system("cls");
+		return;
+	}
+	
+	else
+		goto back3;
 	
 }
 
